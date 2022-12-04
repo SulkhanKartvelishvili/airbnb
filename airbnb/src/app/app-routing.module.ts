6fab2f1157c7c7cc46dec63478b79809cdea3fe1
SulkhanKartvelishvili <sulxani.kartvelishvili@gmail.com>
2022-12-04 14:06:11 +0400

    Update app-routing.module.ts

--- airbnb/src/app/app-routing.module.ts
+++ airbnb/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
 import { HotelInnerComponent } from './shared/shared_components/hotel_inner/hotel-inner/hotel-inner.component';
 import { HomeComponent } from './view/home/home/home.component';
 
@@ -8,14 +7,6 @@ const routes: Routes = [
   {path:"home", component:HomeComponent},
   {path:"innerHotel", component:HotelInnerComponent},
   
-=======
-import { AppComponent } from './app.component';
-import { HotelInnerComponent } from './shared/shared_components/hotel_inner/hotel-inner/hotel-inner.component';
-
-const routes: Routes = [
-
-{path:'hotel_inner', component:HotelInnerComponent}
->>>>>>> Stashed changes
 ];
 
 @NgModule({
