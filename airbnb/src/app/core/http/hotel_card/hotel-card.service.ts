8981dfb68ed3164d2c3ba754ce25c7384b899de8
SulkhanKartvelishvili <sulxani.kartvelishvili@gmail.com>
2022-12-18 00:42:38 +0400

    filter

--- airbnb/src/app/core/http/hotel_card/hotel-card.service.ts
+++ airbnb/src/app/core/http/hotel_card/hotel-card.service.ts
@@ -9,6 +9,7 @@ export class HotelCardService {
 
 
   private getAllHotelCardApi:string = "http://airbnb-dev.us-east-1.elasticbeanstalk.com/api/Hotel";
+  private getAllFilterCardApi:string = "http://airbnb-dev.us-east-1.elasticbeanstalk.com/api/Hotel/filter-by-category";
 
   constructor(private httpClient:HttpClient) { 
   }
@@ -18,6 +19,9 @@ export class HotelCardService {
     readAllHotelCard():Observable<any>{
       
       return this.httpClient.get(this.getAllHotelCardApi);
+    }
+    readAllfilteredHotelCard(queryParams:any):Observable<any>{
+      return this.httpClient.get(this.getAllFilterCardApi, { params: queryParams });
     }
     
     
--- airbnb/src/app/view/category-filter/category-filter.component.ts
+++ airbnb/src/app/view/category-filter/category-filter.component.ts
@@ -5,6 +5,9 @@ import {faSliders} from '@fortawesome/free-solid-svg-icons';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgForm } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HotelCardService } from 'src/app/core/http/hotel_card/hotel-card.service';
 
 
 @Component({
@@ -23,14 +26,17 @@ export class CategoryFilterComponent implements OnInit {
    priceFrom:number = 0;
    priceTo:number =0;
    typeOfPlace!:string;
-
+   rooms!:number;
+   beds!:number;
+   bathrooms!:number;
+   propertyType:string ='';
+   queryParams!:any;
   //  entirePlace:string = '';
   //  privateRoom:string = '';
   //  sharedRoom:string = '';
 
    roomsCounter:number[] =[];
-
-  constructor(private httpClient:HttpClient, private categoryFilterService:CategoryFilterService, private modalService: NgbModal) { }
+  constructor(private httpClient:HttpClient, private categoryFilterService:CategoryFilterService, private modalService: NgbModal, private activatedRoute:ActivatedRoute, private hotelCardServ:HotelCardService) { }
 
   ngOnInit(): void {
 
@@ -73,10 +79,25 @@ AddFilterRooms(){
  this.roomsCounter.push(1);
 
 }
-showresults(){
-  this.close();
+onHomePropertyTypeClick(){
+  this.propertyType = 'home'
 }
-
-
+onVillaProperyTypeClick(){
+  this.propertyType = 'villa';
+}
+showresults(){
+  this.activatedRoute.queryParams.subscribe(response => {
+   this.queryParams = response;
+  })
+  console.log(this.queryParams);
+  // this.close();
+ 
+  this.hotelCardServ.readAllfilteredHotelCard(this.queryParams);
+  this.close();
+  this.hotelCardServ.readAllHotelCard().subscribe(response => {
+    console.log(response);
+  })
+  
+}
 
 }
